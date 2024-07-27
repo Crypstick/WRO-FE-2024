c9213cc79cf24f4f9d42ae63cce8f82d35bfd1df
Crypstick <88693462+Crypstick@users.noreply.github.com>
2024-07-27 20:56:55 +0800

    Creation of Template + Initialisation Code

---
+++ src/wro_draft1/wro_draft1.ino
@@ -0,0 +1,100 @@
+#include <Evo.h>
+#include <Tones.h>
+#include <Magnometer.h>
+#include <EV3Motor.h>
+
+
+
+#define magnometerPin 1
+#define leftSensor 4
+#define rightSensor 5
+
+
+EVO evo;
+// Declares Motor and selects port (M1-4), flipped?
+EV3Motor steeringMotor(M1, true);
+Ev3Motor drivingMotor(M2, true);
+
+// Declares Magnometer
+Magnometer magnometer(I2C2);
+int maxSteer;
+
+
+//The number of ToF sensors
+const int sensorCount = 2;
+
+// associated I2C channel to the sensors
+const int sensors[sensorCount] = {leftSensor, rightSensor}; // TO CHANGE ASAP
+
+int leftSensorReading = 0;
+int rightSensorReading = 0;
+
+void steerToAngle(int angle, int time);
+void resetSteering();
+int limit(int value, int min, int max);
+
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(115200);
+  evo.begin();
+  evo.beginDisplay();
+  // Initializes motor
+  steeringMotor.begin();
+  drivingMotor.begin();
+  // Initalises Magnometer
+  magnometer.begin();
+  magnometer.setTargetHeading();
+  // Reset encoder
+  steeringMotor.resetAngle();
+  drivingMotor.resetAngle();
+
+  //Gets battery Level and write to display
+  Serial.print("Battery Voltage: ");
+  Serial.println(evo.getBattery());
+  Serial.println("Do not let batteries go below 6.0V");
+  evo.writeToDisplay("Battery", 0, true);
+  evo.writeToDisplay(evo.getBattery(), 0);
+
+  //Plays the buzzer for 300ms
+  evo.playTone(NOTE_G4, 300);
+
+  steeringMotor.brake();
+  drivingMotor.brake();
+
+}
+
+void loop() {
+  
+  for (int i = 0; i < sensorCount; i++) {
+    
+    evo.selectI2CChannel(sensors[i]);
+    evo.read(sensors[i]);
+  }
+
+}
+
+void steerToAngle(int angle, int time) {
+  int now = millis();
+  while ((millis() - now) <time) {
+    steer.run((steer.getAngle()-angle)*-5);
+  }
+  steer.brake();
+}
+
+void resetSteering() {
+  steeringMotor.run(-100);
+  delay(1500);
+  steeringMotor.resetAngle();
+  steeringMotor.run(100);
+  delay(2000);
+  maxSteer = steer.getAngle();
+  steerToAngle(maxSteer/2, 1000);
+  steer.resetAngle();
+  delay(1000);
+}
+
+int limit(int value, int min, int max) {
+  if (value < min) return min;
+  else if (value > max) return max;
+  else return value;
+}
