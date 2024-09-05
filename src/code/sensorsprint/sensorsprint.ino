c4bacb0058bdc3e0181e2dcfce3a36cb847c095a
Crypstick <88693462+Crypstick@users.noreply.github.com>
2024-09-05 10:46:18 +0800

    updat sensor

--- src/code/sensorsprint/sensorsprint.ino
+++ src/code/sensorsprint/sensorsprint.ino
@@ -7,42 +7,33 @@
 #include "Adafruit_TCS34725.h"
 
 #include <SoftwareSerial.h>
-<<<<<<< HEAD
-#include "Adafruit_TCS34725.h"
-=======
 #include <HUSKYLENS.h>
 
->>>>>>> ccf969901de47a367d2e33256039bed02f6ab2f2
 
 #define TCA9548A_ADDR 0x70
 #define BQ25887_ADDR 0x6A
 
-<<<<<<< HEAD
 #define WHITE 0
 #define ORANGE 1
 #define BLUE 2
 
-=======
 #define REDBLOCK 1
 #define GREENBLOCK 2
 
 
 HUSKYLENS huskylens;
 SoftwareSerial mySerial(43, 44);  // RX, TX REMMBER TO FLIP BACK TO 43, 44
->>>>>>> ccf969901de47a367d2e33256039bed02f6ab2f2
 EVO evo;
 EV3Motor steer_motor(M1, true);
 EV3Motor drive_motor(M2, false);
 EV3Motor turnstile_motor(M4, false);
 MPU gyro;
-<<<<<<< HEAD
+
 //VL53L0X distance_front(4);
 VL53L0X distance_left(2);
 VL53L0X distance_right(3);
-
-HUSKYLENS huskylens;
-SoftwareSerial mySerial(43, 44); // RX, TX REMMBER TO FLIP BACK TO 43, 44
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
 const int COLOR_PIN = 4;
 
 int gammatable[256];
@@ -52,30 +43,17 @@ struct colorCalibration {
   unsigned int blackValue;
   unsigned int whiteValue;
 };
+
 colorCalibration redCal, greenCal, blueCal;
 
 
-=======
-//VL53L0X distance_front(1);
-VL53L0X distance_left(2);
-VL53L0X distance_right(3);
 
->>>>>>> ccf969901de47a367d2e33256039bed02f6ab2f2
 HUSKYLENSResult relevantObject;
 
 
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
-
-struct colorCalibration {
-  unsigned int blackValue;
-  unsigned int whiteValue;
-};
-
-int gammatable[256];
-int RGBmap(unsigned int x, unsigned int inlow, unsigned int inhigh, int outlow, int outhigh);
-
+bool findRelevantObject();
+void printResult(HUSKYLENSResult result);
 // initiate three stuctures to hold calibration of Red, Green and LED's in TCS3472
-colorCalibration redCal, greenCal, blueCal;
 
 
 
@@ -111,30 +89,28 @@ void setup() {
     delay(100);
   }
 
-<<<<<<< HEAD
+
   evo.selectI2CChannel(COLOR_PIN);
-=======
+
   evo.selectI2CChannel(4);
->>>>>>> ccf969901de47a367d2e33256039bed02f6ab2f2
+
   if (tcs.begin()) {
     Serial.println("Found sensor");
   } else {
     Serial.println("No TCS34725 found ... check your connections");
     while (1);
   }
-<<<<<<< HEAD
-=======
 
   Serial.println("TCS3472 OK");
 
     // These values result from calibration of the TCS3472 looking at a black sample and a white sample
   // Must be edited here once calibration values have been determined
   redCal.blackValue = 33;
-  redCal.whiteValue = 198;
-  greenCal.blackValue = 23;
-  greenCal.whiteValue = 209;
-  blueCal.blackValue = 15;
-  blueCal.whiteValue = 158;
+  redCal.whiteValue = 235;
+  greenCal.blackValue = 25;
+  greenCal.whiteValue = 245;
+  blueCal.blackValue = 20;
+  blueCal.whiteValue = 188;
 
   /* white
   Color Temp: 4431 K - Lux: 149 - R: 198 G: 209 B: 158 C: 591  
@@ -145,7 +121,7 @@ void setup() {
   // Required to correct for human vision
   // Store values in an array
   // normalized for 0 to 255
->>>>>>> ccf969901de47a367d2e33256039bed02f6ab2f2
+
   for (int i=0; i<256; i++) {
     float x = i;
     x /= 255;
@@ -153,21 +129,24 @@ void setup() {
     x *= 255;
 
     gammatable[i] = int(x);
-<<<<<<< HEAD
+
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print(gyro.getHeading());
-  //Serial.print(", front dist: ");
-  //Serial.print(distance_front.getDistance());
+  Serial.println(gyro.getHeading());
+  // Serial.print(", front dist: ");
+  // Serial.print(distance_front.getDistance());
+  /*
   Serial.print(", left dist: ");
-  Serial.print(distance_left.getDistance());
+  Serial.println(distance_left.getDistance());
   Serial.print(", right dist: ");
-  Serial.print(distance_right.getDistance());
+  Serial.println(distance_right.getDistance());
   Serial.print(", color: ");
-  Serial.print(getColor(false));
+  */
+  Serial.println(getColor(true));
+  
   Serial.print(", camera: ");
   if (findRelevantObject()) {
     printResult(relevantObject);
@@ -176,68 +155,12 @@ void loop() {
     int area = relevantObject.height * inWidth;
     if (inWidth < 80 || area < 6000 || inWidth > length) Serial.println("tracking");
     else Serial.println("hardcode");
+  } else { Serial.println("No relevent object!");
   }
-  else Serial.println("No relevent object!");
-=======
-    }
-}
+} 
 
-void loop() {
-  int bingbong = distance_right.getDistance();
-  Serial.print("distance right: ");
-  Serial.println(bingbong);
+// 90 7 270 187
 
-  uint16_t r, g, b, c; // raw values of r,g,b,c as read by TCS3472
-  // Variables used to hold RGB values between 0 and 255
-  int redValue;
-  int greenValue;
-  int blueValue;
-  int clearValue;
-  // colorTemp = tcs.calculateColorTemperature(r, g, b);
-  evo.selectI2CChannel(4);
-  delay(10);
-  tcs.getRawData(&r, &g, &b, &c);
-  
-  // Print out raw data resulting from read cycle, use to calibrate bias value
-  Serial.print("R: "); Serial.print(r); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c); Serial.print(" ");
-  Serial.println(" ");
-
-  delay(50);
-
-  // Convert TCS3472 raw reading into value between 0 and 255 for analogwrite function
-  redValue = RGBmap(r, redCal.blackValue, redCal.whiteValue, 0, 255);
-  greenValue = RGBmap(g, greenCal.blackValue, greenCal.whiteValue, 0, 255);
-  blueValue = RGBmap(b, blueCal.blackValue, blueCal.whiteValue, 0, 255);
-
-  // Print out values
-  Serial.print("RValue: "); Serial.print(redValue); Serial.print(" ");
-  Serial.print("GValue: "); Serial.print(greenValue); Serial.print(" ");
-  Serial.print("BValue: "); Serial.print(blueValue); Serial.print(" ");
-  Serial.println(" ");
-
-  
-
->>>>>>> ccf969901de47a367d2e33256039bed02f6ab2f2
-}
-
-// Function to map TCS3472 values to 0 to 255
-// Same as Arduino map() function - rewritten to make variables compatiable with inputs and outputs
-int RGBmap(unsigned int x, unsigned int inlow, unsigned int inhigh, int outlow, int outhigh){
-  float flx = float(x);
-  float fla = float(outlow);
-  float flb = float(outhigh);
-  float flc = float(inlow);
-  float fld = float(inhigh);
-
-  float res = ((flx-flc)/(fld-flc))*(flb-fla) + fla;
-
-  return int(res);
-}
-
-<<<<<<< HEAD
 int getColor(bool printRaw) {
   uint16_t r, g, b, c; // raw values of r,g,b,c as read by TCS3472
   // Variables used to hold RGB values between 0 and 255
@@ -260,9 +183,10 @@ int getColor(bool printRaw) {
     Serial.print("\t : ");
   }
 
+  if (redValue + greenValue + blueValue > 600) return WHITE;
   if (redValue > greenValue && redValue > blueValue) return ORANGE;
   else if (blueValue > redValue && blueValue > greenValue) return BLUE;
-  else return WHITE;
+  else return ORANGE;
 
 }
 
@@ -278,9 +202,7 @@ int RGBmap(unsigned int x, unsigned int inlow, unsigned int inhigh, int outlow, 
   return int(res);
 }
 
-bool findRelevantObject() {
-=======
->>>>>>> ccf969901de47a367d2e33256039bed02f6ab2f2
+
 
 void initialiseSteering() {
   steer_motor.resetAngle();
@@ -294,4 +216,39 @@ void initialiseSteering() {
   while (steer_motor.getAngle() > a) {}
   steer_motor.resetAngle();
   steer_motor.coast();
+}
+
+bool findRelevantObject() {
+
+  if (!huskylens.request()) {
+
+    Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
+    return false;
+  } else if (!huskylens.available()) {  // no block on the screen, keep to current dir
+    return false;
+  } else {
+    int biggestY = -1;
+    HUSKYLENSResult candidate;
+    for (int i = 0; i < huskylens.countBlocks(); i++) {
+      HUSKYLENSResult current = huskylens.getBlock(i);
+      if (current.width > 35 && current.width < 160) {
+        if (current.yCenter > biggestY) {
+          biggestY = current.yCenter;
+          candidate = current;
+        }
+      }
+    }
+    if (biggestY != -1) {
+      relevantObject = candidate;
+      return true;
+    } else return false;
+  }
+}
+
+void printResult(HUSKYLENSResult result) {
+  if (result.command == COMMAND_RETURN_BLOCK) {
+    Serial.println(String() + F("Block:xCenter=") + result.xCenter + F(",yCenter=") + result.yCenter + F(",width=") + result.width + F(",height=") + result.height + F(",ID=") + result.ID);
+  } else {
+    Serial.println("Object unknown!");
+  }
 }
